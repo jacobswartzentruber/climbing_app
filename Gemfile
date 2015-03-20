ac4984609ed3664bb6c5e914d41bc94f4e06a813
Jacob Swartzentruber <jsswart@gmail.com>
2015-03-20 15:24:46 +0000

    Added edit and delete for users

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'bootstrap-sass'
 gem 'bcrypt', '3.1.7'
+gem 'faker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
   def index
     @users = User.all
@@ -24,6 +25,25 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+  
   private
     
     def user_params
@@ -31,7 +51,12 @@ class UsersController < ApplicationController
     end
     
     def correct_user
-      @user = User.find_by(params[:id])
+      @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
+    end
+    
+    def admin_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless @user.admin?
     end
 end
