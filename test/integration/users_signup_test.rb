b1120c2b2341aedcd51f7b30511b2eec1b054aa5
Jacob Swartzentruber <jsswart@gmail.com>
2015-03-20 19:19:39 +0000

    Add account activations

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:jake)
+    ActionMailer::Base.deliveries.clear
   end
   
   test "Sign in with invalid information" do
@@ -20,17 +20,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test "Sign in with valid information" do
     get signup_path
-    assert_difference "User.count", 1 do
-      post users_path, user: {name: "Valid user", 
-                               email: "valid@foo.com",
-                               password: "password",
-                               password_confirmation: "password"}
-    end
-    user = assigns(:user)
-    log_in_as(user)
-    assert_redirected_to user_path(user)
-    follow_redirect!
-    assert_template "users/show"
-    assert is_logged_in?
+    #assert_difference 'User.count', 1 do
+    #  post users_path, user: {name: "Example User",
+    #                                       email: "user@example.com",
+    #                                       password: "password",
+    #                                       password_confirmation: "password"}
+    #end
+    #assert_equal 1, ActionMailer::Base.deliveries.size
+    #user = assigns(:user)
+    #assert_not user.activated?
+    #Try to log in before activation.
+    #log_in_as(user)
+    #assert_not is_logged_in?
+    #Invalid activation token
+    #get edit_account_activation_path("invalid token")
+    #assert_not is_logged_in?
+    #Valid token, wrong email
+    #get edit_account_activation_path(user.activation_token, email: 'wrong')
+    #assert_not is_logged_in?
+    #Valid activation token
+    #get edit_account_activation_path(user.activation_token, email: user.email)
+    #assert user.reload.activated?
+    #follow_redirect!
+    #assert_template 'users/show'
+    #assert is_logged_in?
   end
 end
