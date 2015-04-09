8bd7bffaa71054eaa0531c5d0364f1a351366cf8
Jacob Swartzentruber <jsswart@gmail.com>
2015-04-09 21:59:10 +0000

    Transitioning last changes from Cloud9 to local env

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,6 +2,7 @@ class Route < ActiveRecord::Base
   belongs_to :gym
   
   validates_presence_of :gym_id
+  validates :color, presence: true
   validates :difficulty, presence: true
   validates :description, length: {maximum: 140}
 end
